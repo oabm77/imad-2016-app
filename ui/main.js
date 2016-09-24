9148b73827d6c26fb3a467dd618d7f62f7514a44
Omar Augusto Bautista Mora <oabm77@users.noreply.github.com>
2016-09-24 03:22:31 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,31 +10,54 @@ button.onclick = function() {
     if(request.readyState === XMLHttpRequest.DONE) {
         // Take some action
         if (request.status === 200) {
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
+            // Capture a list of names and render it as a list
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list = '';
+            for(var i=0; i < names.length; i++) {
+                list += '<li>'+ names[i] + '</li>';
+            }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
         }
     }
     // Not done yet
   };
   
   // Make a request
-  request.open('GET', 'http://oabm77.imad.hasura-app.io/counter', true);
+  request.open('GET', 'http://oabm77.imad.hasura-app.io/submit-name?name=' + name, true);
   request.send(null);
 };
 
 // submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
-  // Make a request to the server and send a name
-  // Capture a list of names and render it as a list
-  var names = ['name1', 'name2', 'name3', 'name4'];
-  var list = '';
-  for(var i=0; i < names.length; i++) {
-      list += '<li>'+ names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
-};
+  // Create a request object
+  var request = new XMLHttpRequest();
+  
+  // Capture the response and store it in a variable
+  request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            // Take some action
+            if (request.status === 200) {
+                // Capture a list of names and render it as a list
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0; i < names.length; i++) {
+                    list += '<li>'+ names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+        // Not done yet
+   };
+   // Make the request 
+   var nameInput = document.getElementById('name');
+   var name = nameInput.value;
+   request.open('GET', 'http://oabm77.imad.hasura-app.io/submit-name?name=' + name, true);
+   request.send(null);
+}
+
+
