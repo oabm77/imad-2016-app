cffd1dafc18ddf1778b7a1c16aeb3d1656131f2e
Omar Augusto Bautista Mora <oabm77@users.noreply.github.com>
2016-09-24 03:06:17 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,6 +84,16 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name', function(req, res) { // URL: submit-name?name=xxxxx
+   // Get the name from the request
+   var name = req.query.name;
+   
+   names.push(name);
+   // JSON: JavaScript Object Notation
+   res.send(JSON.stringify(names)); // TODO
+});
+
 app.get('/:articleName', function(req, res) {
     // articleName == article_one
     // articles[articleName] == {} content object for article one
@@ -101,16 +111,6 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-var names = [];
-app.get('/submit-name', function(req, res) { // URL: submit-name?name=xxxxx
-   // Get the name from the request
-   var name = req.query.name;
-   
-   names.push(name);
-   // JSON: JavaScript Object Notation
-   res.send(JSON.stringify(names)); // TODO
 });
 
 
